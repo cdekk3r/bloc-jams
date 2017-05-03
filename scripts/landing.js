544ef2c6d58ddc595dc8ec0cc3c9f0bff94fcf77
Corey Dekker <Corey Dekker>
2017-05-02 23:58:35 -0400

    checkpoint-9 done

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,23 +1,28 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-    var revealPoint = function(points) {
-      for(var i - 0; i < points.length; i++) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-      }
-    };
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+  };
 
-    revealPoint();
-
+var animatePoints = function(pointsArray) {
+  for(var i = 0; i < pointsArray.length; i++) {
+      revealPoint(pointsArray[i]);
+    }
 };
 
 window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+}
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
     window.addEventListener("scroll", function(event) {
-       console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-         });
- }
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
