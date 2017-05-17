184770dd4f3690d3d679d450a2a497a57a9068e7
Corey Dekker <Corey Dekker>
2017-05-17 13:51:39 -0400

    assignment 20

--- scripts/album.js
+++ scripts/album.js
@@ -170,6 +170,18 @@ var updatePlayerBarSong = function () {
   $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
+var togglePlayFromPlayerBar = function () {
+  if (currentSoundFile.isPaused()) {
+      songNumberCell.html(playButtonTemplate);
+      $('.main-controls .play-pause').html(playerBarPauseButton);
+      currentSoundFile.play();
+    } else {
+      songNumberCell.html(pauseButtonTemplate);
+      $('.main-controls .play-pause').html(playerBarPlayButton);
+      currentSoundFile.pause();
+    }      
+}
+
 
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -185,11 +197,13 @@ var updatePlayerBarSong = function () {
 
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
+ var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPauseButton.click(togglePlayFromPlayerBar());
 
         });
 
